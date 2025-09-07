2121fa4c6c05062c5196d47fc996a9eb1d6be295
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-06 21:31:34 -0700

    config

---
+++ server/config/cloudinary.js
@@ -0,0 +1,23 @@
+// backend/config/cloudinary.js
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Storage settings
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "megal_water_driller", // your folder name in cloudinary
+    allowed_formats: ["jpg", "png", "jpeg", "mp4", "pdf"], // what you want to allow
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
