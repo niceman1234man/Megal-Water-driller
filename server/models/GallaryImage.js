e61058d87530381274465683a26d1b410b428e9b
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-08 16:14:46 -0700

    git model

--- server/models/GallaryImage.js
+++ server/models/GallaryImage.js
@@ -5,6 +5,7 @@ const GallerySchema = new mongoose.Schema(
     location: { type: String, required: true, trim: true },
     client: { type: String, required: true, trim: true },
     url: { type: String, required: true }, // Path to image/video file
+    public_id:{ type: String},
   },
   { timestamps: true }
 );
