19806bcc327386ba8ce283677ec9cd5e1371d574
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-15 17:59:39 -0700

    updated cloudinary

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -2,6 +2,8 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
+const dotenv = require("dotenv");
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
