43b15d12f9239ed8f3c2fd3ad15db2a4a9582d9e
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-18 12:06:21 -0700

    auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
-module.exports = mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model("User", userSchema);
