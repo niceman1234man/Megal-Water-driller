f4635059f74fe0d2fc1d8d97d338a9acbdb284e0
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-21 12:59:21 -0700

    vite config

--- megal/vite.config.js
+++ megal/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["pdfjs-dist/build/pdf.worker.min.js"], // 👈 tell Vite not to bundle it
+    },
+  },
 })
+
+
