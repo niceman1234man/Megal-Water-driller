c8eba1a827caf704855763a150fd20c2c6bd8980
niceman1234man <yihunietarekegn18@gmail.com>
2025-09-21 17:12:16 -0700

    about

--- megal/src/pages/About.jsx
+++ megal/src/pages/About.jsx
@@ -131,7 +131,7 @@ const AboutUs = () => {
     </p>
 
     {about.licenses && about.licenses.length > 0 ? (
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-6">
         {about.licenses.map((license, index) => (
           <div
             key={index}
